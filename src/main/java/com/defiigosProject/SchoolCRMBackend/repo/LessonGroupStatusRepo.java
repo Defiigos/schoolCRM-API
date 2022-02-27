9244bc4dfc20473711f1301194210833728eb487
Defiigos <igorkoxd@gmail.com>
2022-02-28 05:48:59 +0700

    refactoring repositories

---
+++ src/main/java/com/defiigosProject/SchoolCRMBackend/repo/LessonGroupStatusRepo.java
@@ -0,0 +1,7 @@
+package com.defiigosProject.SchoolCRMBackend.repo;
+
+import com.defiigosProject.SchoolCRMBackend.model.LessonGroupStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonGroupStatusRepo extends JpaRepository<LessonGroupStatus, Long> {
+}
--- src/main/java/com/defiigosProject/SchoolCRMBackend/repo/StudentGroupRepo.java
+++
@@ -1,7 +0,0 @@
-package com.defiigosProject.SchoolCRMBackend.repo;
-
-import com.defiigosProject.SchoolCRMBackend.model.LessonGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentGroupRepo extends JpaRepository<LessonGroup, Long> {
-}
